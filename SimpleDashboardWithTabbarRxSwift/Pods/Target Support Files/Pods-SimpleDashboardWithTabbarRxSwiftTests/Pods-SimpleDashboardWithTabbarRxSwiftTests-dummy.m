b2aefe42edb24d2b632079fec2a44f40d85490b8
Viennarz Curtiz <viennarzc@gmail.com>
2019-06-14 09:49:38 +0800

    Init Pod

---
+++ SimpleDashboardWithTabbarRxSwift/Pods/Target Support Files/Pods-SimpleDashboardWithTabbarRxSwiftTests/Pods-SimpleDashboardWithTabbarRxSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleDashboardWithTabbarRxSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleDashboardWithTabbarRxSwiftTests
+@end
